185ff64c30e6f3031c550bccac5fcf0b4f4ffd93
shaikh ajharuddin <shaikh.ajharuddin@gmail.com>
2025-09-07 19:41:17 +0530

    all

--- intelligentAI/settings.py
+++ intelligentAI/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-278^e3ctn(ch$u$fh(!x4jbmw^o9-@f*h-!5jpp5-)rj8d@+ut
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['intelligentai.onrender.com','*']
 
 
 # Application definition
